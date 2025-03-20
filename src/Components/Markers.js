071e59a6a4b0b0f4edaf913ac3ca368680c87998
Saurabh Uchil <uchilsaurabh@gmail.com>
2025-03-21 01:39:26 +1100

    Display Measurements and Units

--- src/Components/Markers.js
+++ src/Components/Markers.js
@@ -4,9 +4,8 @@ import "../CSS/Markers.css";
 
 export default function Markers({data, filterText}) {
     
-    const [selectedLocation , setSelectedLocation] = useState({});
-    console.log("filterText: "+filterText);
     
+    const [selectedLocation , setSelectedLocation] = useState({});
     
     const handleClick = (location) =>{
         setSelectedLocation({...location});
@@ -28,18 +27,41 @@ export default function Markers({data, filterText}) {
 
     const markers = myArray.map( (location, i) => (
         <div key={i}> 
+       {/* Marker */}
        <AdvancedMarker
          onClick={() => handleClick(location)}
          key={location.name}
          position={{lat:location.latitude, lng: location.longitude}}>
+       
+       {/* Pin */}
        <Pin background={'#FF851B'} glyphColor={'#000'} borderColor={'#000'}/>
        {selectedLocation && selectedLocation.id === location.id && 
+       
+       /* Popup */
        <InfoWindow  onCloseClick={handleCloseCLick} className='popup' position={{lat:location.latitude, lng: location.longitude}}>
         
          <div className='info'>
              <p><span className='title'>Station:</span> {location.name}</p>
              <p><span className='title'>Site:</span> {location.site}</p>
              <p><span className='title'>Portfolio:</span> {location.portfolio}</p>
+             {location.variables.map((variable, i)=>{
+                
+                //Getting Measurement 1
+                let measurement1 = location.data[0][0];
+
+                //Getting Measurement 2 if exists
+                let measurement2 = 0;
+                if(location.variables.length  === 2){
+                    measurement2 = location.data[0][1];
+                }
+
+                return (
+                    <div>
+                        {i === 0 && <p><span className='title'>{variable.long_name} </span>{measurement1} {variable.unit}</p>}
+                        {i === 1 && measurement2 !== 0 && <p><span className='title'>{variable.long_name} </span>{measurement2} {variable.unit}</p>}
+                    </div>);
+             })}
+             <p><span className='title'>Time:</span> {location.data[0][location.data[0].length-1]}</p>
          </div>
          
        </InfoWindow>}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">Maps App</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
+        <a className="navbar-brand" href="#">Maps App</a>
     </div>
     </nav>
   )
